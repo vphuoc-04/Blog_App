9f87491b344ee4a73c049b92bd10170d54f41e2c
vphuoc-04 <vanphuoc240904@gmail.com>
2024-06-13 10:57:36 +0700

    add pagination

--- admin/src/components/User.js
+++ admin/src/components/User.js
@@ -2,16 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import Delete from '../assets/img/delete.png'
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 
 export const User = () => {
   const [listUser, setListUser] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
   const id = useLocation().search;
+  const userPerPage = 8;
 
   useEffect(() => {
     const fetchData = async () => {
       try{
         const res = await axios.get(`users${id}`);
-        setListUser(res.data);
+        const reverseUser = res.data.reverse();
+        setListUser(reverseUser);
+        setPageCount(Math.ceil(res.data.length / userPerPage));
       }
       catch(err){
         console.log(err);
@@ -36,17 +42,25 @@ export const User = () => {
   };
 
   const displayAvatar = (user) => {
-    if (user && user.img) {
-      if (isURL(user.img)) {
+    if(user && user.img){
+      if(isURL(user.img)) {
         return <img src = {user.img} alt = "" />;
       } 
-      else {
+      else{
         return <img src = {`http://localhost:3000/image/${user.img}`} alt = "" />;
       }
-    } else {
+    } 
+    else {
       return null;
     }
   };
+
+  const handlePageClick = (event) => { setCurrentPage(event.selected); }
+
+  const displayUsers = listUser.slice(
+    currentPage * userPerPage,
+    (currentPage + 1) * userPerPage
+  );
 
   return (
     <div className = "userManagement">
@@ -58,7 +72,7 @@ export const User = () => {
           <th>Email</th>
           <th>Actions</th>
         </tr>
-        { listUser.map((users) => (
+        { displayUsers.map((users) => (
             <tr>
               <td> { displayAvatar(users) } </td>
               <td> { users.id } </td>
@@ -74,6 +88,26 @@ export const User = () => {
             </tr>
         ))}
       </table>
+      <ReactPaginate
+        breakLabel = "..."
+        nextLabel = ">"
+        onPageChange = { handlePageClick }
+        pageRangeDisplayed = {1}
+        pageCount = { pageCount }
+        previousLabel = "<"
+        renderOnZeroPageCount = { null }
+        containerClassName = "paginate"
+        pageClassName = "page-item"
+        pageLinkClassName = "page-link"
+        previousClassName = "page-item"
+        previousLinkClassName = "page-link"
+        nextClassName = "page-item"
+        nextLinkClassName = "page-link"
+        breakClassName = "page-item"
+        breakLinkClassName = "page-link"
+        activeClassName = "active"
+        disabledClassName = "disabled"
+      />
     </div>
   )
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import axios from 'axios'
 import moment from 'moment';
+import ReactPaginate from 'react-paginate';
 
 const Home = () => {
   const [ posts, setPosts ] = useState([]);
   const [ admin, setAdmin ] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
   const id = useLocation().search;
+  const postPerPage = 9;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,6 +18,7 @@ const Home = () => {
         const res = await axios.get(`/posts${id}`);
         const reversedPosts = res.data.reverse();
         setPosts(reversedPosts)
+        setPageCount(Math.ceil(res.data.length / postPerPage));
       }
       catch(err){
         console.log(err);
@@ -35,6 +40,13 @@ const Home = () => {
     fetchDataAdmin();
   }, [id]);
 
+  const handlePageClick = (event) => { setCurrentPage(event.selected); }
+
+  const displayPosts = posts.slice(
+    currentPage * postPerPage,
+    (currentPage + 1) * postPerPage
+  );
+
   return (
     <div className = "home">
       <h1 className = "name">
@@ -43,7 +55,7 @@ const Home = () => {
       <h2>Software Engineer</h2>
       <h1 className = "titleNewPost">Tất Cả Bài Viết</h1>
       <div className = "posts">
-        { posts.map((post) => (
+        { displayPosts.map((post) => (
           <div className = "allPost">
             <div className = "newPost" key = { post.id }>
               <Link className = "img" to = {`/post/${post.url}`}>
@@ -66,6 +78,26 @@ const Home = () => {
           </div>
         )) }
       </div>
+      <ReactPaginate
+        breakLabel = "..."
+        nextLabel = ">"
+        onPageChange = { handlePageClick }
+        pageRangeDisplayed = {1}
+        pageCount = { pageCount }
+        previousLabel = "<"
+        renderOnZeroPageCount = { null }
+        containerClassName = "paginate"
+        pageClassName = "page-item"
+        pageLinkClassName = "page-link"
+        previousClassName = "page-item"
+        previousLinkClassName = "page-link"
+        nextClassName = "page-item"
+        nextLinkClassName = "page-link"
+        breakClassName = "page-item"
+        breakLinkClassName = "page-link"
+        activeClassName = "active"
+        disabledClassName = "disabled"
+      />
     </div>
   )
 }
